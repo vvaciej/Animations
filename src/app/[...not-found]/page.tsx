c1b6708edd6993f03027376075f448906418f37e
Maciek Skokowski <maciejskok2004@gmail.com>
2024-03-06 17:36:14 +0100

    added not found page

---
+++ src/app/[...not-found]/page.tsx
@@ -0,0 +1,24 @@
+import Footer from '@/src/components/Footer';
+import Navbar from '@/src/components/Navbar';
+
+const NotFound = () => {
+	return (
+		<div>
+			<Navbar />
+			<main className='min-h-screen w-full flex justify-center'>
+				<div className='flex justify-between items-center w-9/12 h-screen'>
+					<section>
+						<h1 className='text-8xl mb-6'>
+							Błąd <b className='text-8xl text-red font-semibold'>404</b>.
+						</h1>
+						<span className='text-2xl'>Strona nie znaleziona</span>
+					</section>
+					<img className='h-[40rem]' src='https://stamply.pl/_next/image/?url=%2Fimages%2Fcharacters%2Ftoilet-coffee.png&w=640&q=75' alt='' />
+				</div>
+			</main>
+			<Footer />
+		</div>
+	);
+};
+
+export default NotFound;
